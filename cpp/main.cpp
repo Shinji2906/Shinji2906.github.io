831d3a45040e6d3c2b40702f720aaf13dbe2ee1b
Shinji2906 <khoibachay123@gmail.com>
2024-10-22 21:13:40 +0700

    anh long mua hang khong, nha em moi mo Dien May Xanh

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -4,62 +4,65 @@
 
 using namespace std;
 
-struct CDgame
+struct DienMayXanh
 {
-    string Tengame;
+    string Tensanpham;
     int GiaTien;
 };
-// Nhap ten 1 dia CD game
-void NhapCDgame (struct CDgame &cd) {
-    cout << "Nhap ten dia CD game ban muon sell : ";
-    getline(cin, cd.Tengame);
-    cout << "Gia tien cua CD game la : ";
-    cin >> cd.GiaTien;
+// Nhập tên sản phẩm
+void NhapSanPham (struct DienMayXanh &sp) {
+    cout << "Nhap ten san pham ban muon mua : ";
+    getline(cin, sp.Tensanpham);
+    cout << "Gia tien cua san pham la : ";
+    cin >> sp.GiaTien;
     getchar();
 }
-// in số tiền
-string formatNumber(int num) {
+// máy tính tiền sang hàng nghìn vnd
+string MayTinhTien(int gia) {
     stringstream ss;
-    string result;
-    ss << num;
+    string ketqua;
+    ss << gia;
     string str = ss.str();
 
     int count = 0;
     for (int i = str.length() - 1; i >= 0; --i) {
-        result.insert(result.begin(), str[i]);
+        ketqua.insert(ketqua.begin(), str[i]);
         count++;
         if (count == 3 && i != 0) {
-            result.insert(result.begin(), '.');
+            ketqua.insert(ketqua.begin(), '.');
             count = 0;
         }
     }
-    return result;
+    return ketqua;
 }
-// xuất tên đĩa CD và số tiền
-void XuatCDgame (struct CDgame &cd) {
-    cout << "Hien ban dang sell CD game : " << cd.Tengame << endl;
-    cout << "Gia tien cua dia CD game la : " << formatNumber(cd.GiaTien * 1000) << "vnd" << endl;
+// mua 1 sản phẩm
+void MuaSanPham (struct DienMayXanh &sp) {
+    cout << "Hien ban dang mua san pham : " << sp.Tensanpham << endl;
+    cout << "Gia tien cua san pham la : " << MayTinhTien(sp.GiaTien * 1000) << "vnd" << endl;
 }
 
-// nhập nhiều số lượng đĩa CD
-void NhapNhieuCDgame (struct CDgame cd[], int n) {
+// nhập order nhiều sản phẩm
+void NhapNhieuSanPham (struct DienMayXanh sp[], int n) {
     for (int i = 0; i < n; i++) {
-        NhapCDgame(cd[i]);
+        NhapSanPham(sp[i]);
     }
 }
-void XuatNhieuCDgame (struct CDgame cd[], int n) {
+// Mua nhiều sản phẩm cùng 1 lúc
+void MuaNhieuSanPham (struct DienMayXanh sp[], int n) {
     for (int i = 0; i < n; i++) {
-        XuatCDgame(cd[i]);
+        MuaSanPham(sp[i]);
     }
 }
 
 
 int main () {
     int n;
+    cout << "Vui long nhap so luong san pham ban muon mua tu DienMayXanh : ";
     cin >> n;
     getchar();
-    struct CDgame cd[n];
-    NhapNhieuCDgame(cd, n);
-    XuatNhieuCDgame(cd, n);
+    struct DienMayXanh sp[n];
+    NhapNhieuSanPham(sp, n);
+    MuaNhieuSanPham(sp, n);
+    cout << "Da mua thanh cong!!!";
     return 0;
 }
